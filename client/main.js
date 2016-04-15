5a60f697468ad6dc8ba2d6dc42372911ce13fd4c
Paul Ellsworth <pellswo@us.ibm.com>
2016-04-14 19:31:22 -0700

    add dragula

--- client/main.js
+++ client/main.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import swimlanes from '../imports/components/swimlanes/swimlanes';
+var angularDragula = require('angular-dragula');
 
 
 angular.module('counterpoint', [
   angularMeteor,
+  angularDragula(angular),
   swimlanes.name
 ]);
