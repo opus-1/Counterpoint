66d5939f85b0f50367159a4f510bb3107beeb876
Paul Ellsworth <pellswo@us.ibm.com>
2016-04-15 08:56:25 -0700

    add a bit more...

--- client/app.js
+++ client/app.js
@@ -1,10 +1,11 @@
 var app = angular.module('counterpoint', [
 	'angular-meteor',
 	'ui.router',
-  'ngMaterial'
+  'ngMaterial',
+	'kanban'
 	]);
-	
-	app.config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+
+app.config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
   $locationProvider.html5Mode({
     enabled: true,
     requireBase: false
@@ -14,9 +15,16 @@ var app = angular.module('counterpoint', [
       url: '/',
       templateUrl: 'client/templates/main.html',
       controller: 'MainCtrl'
+    })
+		.state('main.swimlanes', {
+      url: '/kanban',
+      templateUrl: 'client/templates/swimlanes.html',
+      controller: 'SwimlanesCtrl'
     });
 
   $urlRouterProvider.otherwise("/");
+});
 
+app.controller('MainCtrl', function($scope) {
 
 });
