4bb500ff6aebad010632c0ee35d4ff363c127b02
Paul Ellsworth <pellswo@us.ibm.com>
2016-04-15 18:02:38 -0700

    more work... started looking at angular-material layouts...

--- server/seed.js
+++ server/seed.js
@@ -3,15 +3,35 @@ if (Meteor.isServer) {
     if(Tasks.find().count() === 0){
       var tasks = [
         {
-          'name': 'Fix runtime exception'
+          'name': 'Fix runtime exception',
+          'swimlane': 'Backlog' // TODO: Use IDs?  Or what?
         }, {
-          'name': 'Configure Puma'
+          'name': 'Configure Puma',
+          'swimlane': 'Backlog' // TODO: Use IDs?  Or what?
         }, {
-          'name': 'Fix Websocket connection issue'
+          'name': 'Fix Websocket connection issue',
+          'swimlane': 'Backlog' // TODO: Use IDs?  Or what?
         }
       ];
       for(var i = 0; i < tasks.length; i++){
         Tasks.insert(tasks[i]);
+      }
+    }
+
+    if(Swimlanes.find().count() === 0){
+      var swimlanes = [
+        {
+          'name': 'Backlog'
+        }, {
+          'name': 'Development'
+        }, {
+          'name': 'Test'
+        }, {
+          'name': 'Complete'
+        }
+      ];
+      for(var i = 0; i < swimlanes.length; i++){
+        Swimlanes.insert(swimlanes[i]);
       }
     }
 
