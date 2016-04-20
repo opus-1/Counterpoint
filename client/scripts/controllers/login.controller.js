1ceaea92505bb38d39bf4e94e778ad2053891505
Paul Ellsworth <pellswo@us.ibm.com>
2016-04-19 17:45:35 -0700

    update LDAP stuff for bluepages

--- client/scripts/controllers/login.controller.js
+++ client/scripts/controllers/login.controller.js
@@ -1,13 +1,14 @@
 
 angular.module('counterpoint').controller('LoginCtrl', ['$scope', function ($scope) {
 
-  $scope.login = function () {
-    
+  $scope.login = function() {
+    // mmmm
     Meteor.loginWithLDAP($scope.username, $scope.password, {
       // search by email (all other options are configured server side)
       searchBeforeBind: {
-        mail: $scope.username
-      }
+        emailAddress: $scope.username
+      },
+      searchDN: "ou=people,o=ibm.com",
     }, function (err) {
       if (err) {
         console.log(err);
@@ -16,7 +17,6 @@ angular.module('counterpoint').controller('LoginCtrl', ['$scope', function ($sco
         console.log("login successfully");
       }
     });
-    
   };
 
   $scope.register = function () {
--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 //on the server
 Meteor.startup(function () {
 	LDAP_DEFAULTS.url = "ldap://bluepages.ibm.com";
-	LDAP_DEFAULTS.createNewUser = false;
-	
+	LDAP_DEFAULTS.createNewUser = true;
+	LDAP_DEFAULTS.base = "ou=bluepages,o=ibm.com";
+	LDAP_DEFAULTS.searchResultsProfileMap = [
+		{
+	  	resultKey: 'cn',
+	  	profileProperty: 'name'
+		}, {
+		  resultKey: 'serialNumber',
+		  profileProperty: 'serial'
+		}
+	];
+
 	Accounts.config({
 		forbidClientAccountCreation: true
 	});
