e0924ae9a3346aff2944ac3dbc481a673c77a248
Paul Ellsworth <pellswo@us.ibm.com>
2016-04-22 13:05:20 -0700

    make it easier to run the container...

---
+++ docker-compose-up.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Making sure you have docker-compose."
+if which docker-compose 2>&1 1>/dev/null
+then
+  echo "  ... found docker-compose, continuing..."
+else
+  echo "  ... could not find docker-compose!"
+  exit 1
+fi
+
+export COUNTERPOINT_PORT=$1
+if [ -z "$COUNTERPOINT_PORT" ]
+then
+  read -p "Please enter the external port: " port
+fi
+if [ -z "$COUNTERPOINT_PORT"]
+then
+  echo " ... invalid port specified!"
+  exit 1
+fi
+
+if [ -z "$2" ]
+then
+  export COUNTERPOINT_HOSTNAME=$(hostname)
+else
+  export COUNTERPOINT_HOSTNAME=$2
+fi
+
+echo "Running docker-compose."
+docker-compose
